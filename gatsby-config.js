6a8f406c65bcc4986259c62f51bb977294a06cba
Volodia Dubanov <xdatar@outlook.com>
2023-03-10 05:51:19 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my_gatsby_site_2303`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
